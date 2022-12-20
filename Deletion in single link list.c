de96e0b3cf30d15bdd7fc1d9cb7a89476641edbd
Ishita.Srivastava <isha.sri2002@gmail.com>
2022-12-20 19:58:21 +0530

    Create Deletion in single link list.c

---
+++ Deletion in single link list.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{   int info;
+    struct node*next;
+};
+
+int main()
+{
+    int ch,n1,c1,x,p,count=0;
+    struct node *head,*temp,*prev,*nw;
+    
+    //making first node
+    nw=(struct node*)malloc(sizeof(struct node));
+    if(nw==NULL)
+    {   printf("memory is not allocated.");
+    }
+    else
+    {
+        printf("enter first node: ");
+        scanf("%d", &n1);
+        nw->info=n1;
+        nw->next=NULL;
+        head=nw;
+    }
+    
+    //making list
+    do
+    {   
+        struct node*b1;
+        b1=(struct node*)malloc(sizeof(struct node));
+        if(b1==NULL)
+        {   printf("memory is not allocated.");
+                        
+        }
+        else
+        {
+            printf("enter next value: ");
+            scanf("%d", &x);
+            b1->info=x;
+            b1->next=NULL;
+            temp=head;
+            while(temp->next!=NULL)
+            {
+                temp=temp->next;
+            }
+            temp->next=b1;
+        }
+        printf("\nPress 1 for inserting more and 0 to stop.");
+        scanf("%d", &c1);
+    }while(c1==1);
+    printf("the list is: ");
+    for(temp=head;temp!=NULL;temp=temp->next)
+    {
+        printf("%d ", temp->info);
+        count++;
+    }
+    
+    printf("\n1. delete an element from begining.\n2. delete an element from end.\n3. delete an element from given position.\n4. exit. ");
+    scanf("%d", &ch);
+    
+    switch(ch)
+    {
+        case 1: //delete from begining
+                temp=head;
+                head=temp->next;
+                temp->next=NULL;
+                free(temp);
+                printf("the list is: ");
+                for(temp=head;temp!=NULL;temp=temp->next)
+                {
+                    printf("%d ", temp->info);
+                }
+                break;
+                
+        case 2: //delete from end
+                temp=head;
+                while(temp->next!=NULL)
+                {
+                    prev=temp;
+                    temp=temp->next;
+                }
+                prev->next=NULL;
+                temp->next=NULL;
+                free(temp);
+                printf("the list is: ");
+                for(temp=head;temp!=NULL;temp=temp->next)
+                {
+                    printf("%d ", temp->info);
+                }
+                break;
+                
+        case 3: //delete from given position
+                printf("\nenter the position: ");
+                scanf("%d", &p);
+                if(p>count)
+                    printf("\ninvalid position");
+                else
+                {
+                    temp=head;
+                    for(int i=0;i<p-1;i++)
+                    {
+                        prev=temp;
+                        temp=temp->next;
+                    }
+                    prev->next=temp->next;
+                    temp->next=NULL;
+                    free(temp);
+                }
+                printf("\nthe list is: ");
+                for(temp=head;temp!=NULL;temp=temp->next)
+                {
+                    printf("%d ", temp->info);
+                }
+                break;
+                
+        case 4: break;
+        
+        default: printf("\ninvalid entry.");
+                break;
+    }
+
+    return 0;
+}
